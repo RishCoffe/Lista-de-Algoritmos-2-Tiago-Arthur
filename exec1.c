4ec60152692b4672bdd120c5d8e63d68ce8e3abf
RishCoffe <tiagoavj@gmail.com>
2025-11-12 00:05:48 -0300

    Update exec1.c

---
+++ exec1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int dobro(int valor);
+
+int main()
+{
+    int num;
+    
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+    printf("O dobro de %d eh %d. \n", num, dobro(num));
+    
+    return 0;
+}
+
+int dobro(int valor)
+{   
+    
+    
+    return (2 * valor);
+}
