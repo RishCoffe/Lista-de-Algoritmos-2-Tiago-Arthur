82502ee2fef353c7489f09cc80ff6b99e25e05f8
RishCoffe <tiagoavj@gmail.com>
2025-11-12 00:06:59 -0300

    Update exec24.c

--- exec24.c
+++ exec24.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 
-int main()
-{
+void triangulo_central(int n) {
+    if (n <= 0) {
+        printf("Valor invalido! Digite um numero maior que zero.\n");
+        return;
+    }
+
+    for (int i = 1; i <= n; i++) {
     
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
     
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Digite o valor de n: ");
+    scanf("%d", &n);
+
+    triangulo_central(n);
+
     return 0;
 }
